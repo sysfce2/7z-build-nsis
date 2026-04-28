a5b14e07b818daff1b19a8b032ff7d82e949e9fe
myfreeer <myfreeer@users.noreply.github.com>
2026-04-28 19:11:07 +0800

    build: 7-zip 26.01
    https://github.com/myfreeer/7z-build-nsis/issues/69
    
    
    26.01          2026-04-27
    -------------------------
    - linux version of 7-Zip can use huge pages (2 MB pages). It can increase compression
      speed for 10% for 7z/xz/LZMA/LZMA2 compression.
    - new -spo[d|c|r] switch specifies the path generation mode for the output directory
      for archive extraction. The output directory path is generated from the path specified
      in the -o{dir_path} switch and the name of the archive being unpacked.
        -spod : for Linux/Posix/macOS: -o{dir_path} specifies the direct path to the output directory.
                The asterisk (*) character in {dir_path} will not be replaced by the archive name.
        -spoc : 7-Zip will concatenate the path specified in -o{dir_path} with the archive name
                to form the final path to the output directory.
        -spor : 7-Zip will replace asterisk (*) character in the path specified in the -o{dir_path}
                with the archive name. This is the default option.
    - some bugs were fixed.

--- 7-zip-build.bat
+++ 7-zip-build.bat
@@ -6,7 +6,7 @@ set "Build_Root=%~dp0"
 :Init
 rem 7-zip version
 rem https://www.7-zip.org/
-set version=7z2600
+set version=7z2601
 rem https://github.com/mcmilk/7-Zip-zstd
 set zstd_version=25.01-v1.5.7-R4
 
